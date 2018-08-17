e20fb3fe98c4e22d8ca5b92e88e0f9539d34d396
kleinfreund <phrudloff@gmail.com>
2018-08-17 18:44:52 +0200

    Warn for potential route clashes

--- app/models/route.rb
+++ app/models/route.rb
@@ -16,9 +16,14 @@ class Disraptor::Route
 
     # Adds a new route
     def add(route_id, source_path, target_url, wildcard)
-      if (source_path.start_with?('/admin'))
+      if source_path.start_with?('/admin')
         Rails.logger.error "😱 Attempt to add admin route '#{source_path}' was cancelled."
         return
+      end
+
+      result = Discourse::Application.routes.routes.find { |route| route.ast.to_s.start_with?(source_path) }
+      if result
+        Rails.logger.warn "⚠ Careful! The new route for '#{source_path}' may interfer with the route for '#{result.ast.to_s.chomp('(.:format)')}'."
       end
 
       route = create_route_object(route_id, source_path, target_url, wildcard)
@@ -30,14 +35,10 @@ class Disraptor::Route
 
     # Updates an existing route
     def edit(route_id, source_path, target_url, wildcard)
-      route = create_route_object(route_id, source_path, target_url, wildcard)
-
       # Remove the existing route
       Disraptor::RouteStore.remove_route(route_id)
-      # And add the new one
-      Disraptor::RouteStore.add_route(route_id, route)
 
-      route
+      add(route_id, source_path, target_url, wildcard)
     end
 
     # Removes an existing route
