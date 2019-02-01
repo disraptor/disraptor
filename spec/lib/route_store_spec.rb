170ef39826e3ecc2b6a9d09958adf8648ff33c23
kleinfreund <philipprudloff@fastmail.com>
2019-02-01 14:06:06 +0100

    Finish RouteStore tests

--- spec/lib/route_store_spec.rb
+++ spec/lib/route_store_spec.rb
@@ -31,7 +31,31 @@ describe Disraptor::RouteStore do
     end
   end
 
-  describe ".has_route('1')" do
+  describe ".get_route(id)" do
+    it "should return nil" do
+      expect(described_class.get_route('1')).to be nil
+    end
+
+    it "should return the route" do
+      route = {
+        'id' => '1',
+        'sourcePath' => '/test',
+        'targetURL' => 'http://localhost:8080/test',
+        'requestMethod' => 'get',
+        'segments' => []
+      }
+
+      routes = {
+        '1' => route
+      }
+
+      PluginStore.set(Disraptor::PLUGIN_NAME, 'routes', routes)
+
+      expect(described_class.get_route('1')).to eq route
+    end
+  end
+
+  describe ".has_route(id)" do
     it "should return false" do
       expect(described_class.has_route('1')).to be false
     end
@@ -50,6 +74,52 @@ describe Disraptor::RouteStore do
       PluginStore.set(Disraptor::PLUGIN_NAME, 'routes', routes)
 
       expect(described_class.has_route('1')).to be true
+    end
+  end
+
+  describe ".add_route(id, route)" do
+    it "should add the route" do
+      route = {
+        'id' => '1',
+        'sourcePath' => '/test',
+        'targetURL' => 'http://localhost:8080/test',
+        'requestMethod' => 'get',
+        'segments' => []
+      }
+
+      described_class.add_route('1', route)
+
+      routes = {
+        '1' => route
+      }
+
+      expect(described_class.get_routes()).to eq(routes)
+    end
+  end
+
+  describe ".remove_route(id)" do
+    it "should remove the route" do
+      expect(described_class.get_routes()).to eq({})
+
+      route = {
+        'id' => '1',
+        'sourcePath' => '/test',
+        'targetURL' => 'http://localhost:8080/test',
+        'requestMethod' => 'get',
+        'segments' => []
+      }
+
+      described_class.add_route('1', route)
+
+      routes = {
+        '1' => route
+      }
+
+      expect(described_class.get_routes()).to eq(routes)
+
+      described_class.remove_route('1')
+
+      expect(described_class.get_routes()).to eq({})
     end
   end
 end
