dbe5c92bda14f6e47eb55836affb5d6b08cf692a
Bastian Grahm <bg40lecu@studserv.uni-leipzig.de>
2021-05-27 13:24:54 +0200

    try without setting content-type header

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -152,12 +152,12 @@ skip_before_action :check_xhr, :verify_authenticity_token
     when 'HEAD'
       return Net::HTTP::Head.new(target_url, proxy_headers)
     when 'POST'
-      proxy_headers['Content-Type'] = request.format.to_s
+#      proxy_headers['Content-Type'] = request.header['Content-Type']
       proxy_request = Net::HTTP::Post.new(target_url, proxy_headers)
       proxy_request.set_form_data(request.request_parameters)
       return proxy_request
     when 'PUT'
-      proxy_headers['Content-Type'] = request.format.to_s
+#      proxy_headers['Content-Type'] = request.header['Content-Type']
       proxy_request = Net::HTTP::Put.new(target_url, proxy_headers)
       proxy_request.set_form_data(request.request_parameters)
       return proxy_request
