244710b56829b9c6531c77df0f8bf57f13992f5e
Maik Fröbe <mam10eks@users.noreply.github.com>
2023-10-06 09:29:25 +0200

    Update __init__.py

--- discourse-client-in-disraptor/discourse_client_in_disraptor/__init__.py
+++ discourse-client-in-disraptor/discourse_client_in_disraptor/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
-from discourse_client_in_disraptor.discourse_api_client import DiscourseApiClient
+from discourse_client_in_disraptor.discourse_api_client import *
 
